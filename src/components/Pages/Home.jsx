ad4c05f33e39a4cf60c6be518e92be2e3fc2b9bd
D'Amore Lorenza <lorenzadamore@outlook.it>
2024-02-26 14:53:06 +0100

    home css

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Card, Col, Container, Row } from "react-bootstrap";
+import { Accordion, Card, Col, Container, Image, Row } from "react-bootstrap";
 import "./css/Home.css"
 import { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
@@ -85,19 +85,30 @@ const functionGetUser = (event) => {
       <>
         <Container className="container">
           <Row>
-          <Col xs={1} md={2} lg={1} xl={2} className="inserimentoCanvas">
-      <Row className="logoutLogin">
+          <Col xs={1} md={2} lg={1} xl={2} className="inserimentoHome">        
+        
+<Row className="logoutLogin ms-4 mb-2">
+          {/* Pulsante Profilo */}
+          {isLoggedIn && (
+            <Button className='iconHomeWrite' onClick={() => navigate("/profile")}>
+              <i className="bi bi-person-circle iconHome"></i>
+              Profilo
+            </Button>
+          )}
+</Row>
+
+      <Row >
             {isLoggedIn && (
-            <Button className='iconCanvasWrite' onClick={() => logout()}>
-              <i className="bi bi-box-arrow-left iconCanvas"></i>
+            <Button className='iconHomeWrite' onClick={() => logout()}>
+              <i className="bi bi-box-arrow-left iconHome"></i>
               Logout
             </Button>
           )}
 
           {/* Pulsante Login */}
           {!isLoggedIn && (
-            <Button className='iconCanvasWrite' onClick={() => navigate("/login")}>
-              <i className="bi bi-box-arrow-in-right iconCanvas"></i>
+            <Button className='iconHomeWrite' onClick={() => navigate("/login")}>
+              <i className="bi bi-box-arrow-in-right iconHome"></i>
               Login
             </Button>
           )}  
@@ -108,30 +119,30 @@ const functionGetUser = (event) => {
               {/* <CentralSection /> */}
 
 
-      <Card  className="offCanvasSection">
+      <Card  className="offHomeSection ms-5">
         <Card.Header>
-          <Card.Title className='OffcanvasTitle'>BENVENUTI A FUTURO IMPERFETTO 2.0 </Card.Title>
+          <Card.Title className='OffHomeTitle ms-5'>BENVENUTI A FUTURO IMPERFETTO 2.0 </Card.Title>
                {/* Pulsante Logout */}
  
           </Card.Header>
         <Card.Body>
-        <i className=" iconCanvas">
-        <Button className='iconCanvasWrite'
+        <i className="iconHome">
+        <Button className='iconHomeWrite'
           onClick={() => {
             navigate("/");
           }}>Home</Button>
           </i>
         </Card.Body>
         <Card.Body>
-        <i className=" iconCanvas">
-        <Button className='iconCanvasWrite'
+        <i className=" iconHome">
+        <Button className='iconHomeWrite'
           onClick={() => {
             navigate("/event");
           }}>Eventi</Button>
           </i>
         </Card.Body>
         <Card.Body>
-        <i className=" iconCanvas"><Button className='iconCanvasWrite'
+        <i className=" iconHome"><Button className='iconHomeWrite'
             onClick={() => {
             navigate("/games");
           }}>Giochi da tavola</Button></i>
@@ -140,7 +151,7 @@ const functionGetUser = (event) => {
 
         <Card.Body>
         <Row>
-        <i className="iconCanvas prenotazioniWrite"><Button className='iconCanvasWrite '
+        <i className="iconHome prenotazioniWriteHome"><Button className='iconHomeWrite '
            onClick={() => {
             navigate("/reservation");
           }}>Prenotazioni </Button></i>
@@ -155,26 +166,50 @@ const functionGetUser = (event) => {
 </Row>
         </Card.Body>
 
-        <Card.Body>
-        
 
-          {/* Pulsante Profilo */}
-          {isLoggedIn && (
-            <Button className='iconCanvasWrite' onClick={() => navigate("/profile")}>
-              <i className="bi bi-person-circle iconCanvas"></i>
-              Profilo
-            </Button>
-          )}
+<Card.Body>
+        <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header className="iconHomeWrite iconHome">Contatti</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+
+</Card.Body>
 
 
 
-        </Card.Body>
       </Card>
 
 
               </div>
             </Col>
-            <Col xs={1} md={1} lg={1} xl={0}></Col>
+            <Col xs={1} md={1} lg={1} xl={1}></Col>
+          </Row>
+<Row>
+return <Image src="holder.js/100px250" fluid />;
+</Row>
+          <Row>
+
+<p>
+Futuro Imperfetto 2.0 nasce il 14 settembre 2014 grazie gruppo di ragazzi che ogni mercoledì sera si riuniva in un garage, spartendosi in parti uguali l’affitto, per giocare da tavolo. 
+Tanto erano presi da questa passione che il garage non bastava più.
+Volevano un luogo accogliente dove riunirsi e che potesse accogliere nuov* giocator*.
+E dove prima c’era una lavanderia hanno immaginato e poi creato il locale che conosciamo oggi.
+Sono passati quasi 10 anni da allora, e a Futuro Imperfetto, la nostra amata nave volante, la passione per il gioco da tavolo è ancora il cuore pulsante del progetto. Cuore pulsante ma non unico motore. 
+Da diversi anni infatti ospitiamo concerti, spettacoli teatrali, presentazioni di libri, esposizioni artistiche, incontri e tavoli di confronto su temi vari.
+I principi che portiamo avanti sono quelli dell'inclusività e della condivisione.
+tutt* sono bene accolti sulla nave volante.
+</p>
+
           </Row>
         </Container>
       </>
